68323293eee61a85a1642e62c7f1a21c3e60d3be
yuch3n <dustchen1126@gmail.com>
2024-01-26 18:43:02 +0800

    updates

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,26 @@
 <template>
   <div class="container py-3">
     <div class="border border-dark rounded p-3">
-      <div class="row">
+      <div class="row g-3">
         <div class="col-12 col-lg-8">
           <div class="d-flex flex-column justify-content-between mb-3">
-            <h1>This is a VueNotePad</h1>
+            <h1 class="fw-bold display-3">This is a VueNotePad</h1>
             <button
               class="btn btn-secondary align-self-end"
               @click="addNewNote"
             >
-              New
+              <i class="fa-solid fa-plus me-1"></i>New
             </button>
           </div>
           <textarea
             class="form-control"
             rows="15"
-            v-model="content"
+            v-for="(note, index) in noteList"
+            v-show="note.isShowed"
+            v-model="note.content"
+            :key="index"
             ref="textarea"
+            placeholder="type here..."
           ></textarea>
         </div>
         <div class="col-12 col-lg-4">
@@ -26,7 +30,17 @@
               v-for="(item, index) in noteList"
               :key="index"
             >
-              {{ item.content }}
+              <div class="d-flex justify-content-between align-items-center">
+                <p class="mb-0 text-secondary">{{ item.content }}</p>
+                <div class="d-flex gap-1">
+                  <button class="btn" @click="selectedNote(index)">
+                    <i class="fa-solid fa-pen-to-square"></i>
+                  </button>
+                  <button class="btn" @click="deleteNote(index)">
+                    <i class="fa-solid fa-trash"></i>
+                  </button>
+                </div>
+              </div>
             </li>
           </ul>
         </div>
@@ -41,20 +55,63 @@ export default {
   data() {
     return {
       content: "",
-      noteList: [],
+      noteList: [
+        {
+          content: "",
+          isShowed: true,
+        },
+      ],
     };
   },
   methods: {
     addNewNote() {
+      this.closeAllNotes();
       const newNote = {
         content: this.content,
+        isShowed: true,
       };
       this.noteList.push(newNote);
       this.content = "";
     },
+    closeAllNotes() {
+      this.noteList.map((item) => {
+        item.isShowed = false;
+      });
+    },
+    selectedNote(idx) {
+      this.closeAllNotes();
+      this.noteList[idx].isShowed = true;
+    },
+    deleteNote(idx) {
+      const del_confirmed = confirm("確認要刪除這則筆記？");
+      if (del_confirmed) {
+        const noteLength = this.noteList.length;
+        if (noteLength == 1) {
+          this.noteList.splice(idx, 1);
+          this.addNewNote();
+        } else {
+          this.noteList.splice(idx, 1);
+          this.closeAllNotes();
+          this.noteList[this.noteList.length - 1].isShowed = true;
+        }
+      } else return;
+    },
+    loadLocalStorage() {
+      localStorage.getItem("noteStorage")
+        ? (this.noteList = JSON.parse(localStorage.getItem("noteStorage")))
+        : this.addNewNote();
+    },
+  },
+  watch: {
+    noteList: {
+      handler() {
+        localStorage.setItem("noteStorage", JSON.stringify(this.noteList));
+      },
+      deep: true,
+    },
   },
   mounted() {
-    autosize(this.$refs.textarea);
+    autosize(this.$refs.textarea), this.loadLocalStorage();
   },
 };
 </script>
