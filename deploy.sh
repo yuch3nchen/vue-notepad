32cc3908f9726bb90bb3a4772ff30888599361d8
yuch3n <dustchen1126@gmail.com>
2024-09-06 17:52:08 +0800

    rebuild gh-pages

--- deploy.sh
+++ deploy.sh
@@ -6,23 +6,19 @@ set -e
 # 打包編譯
 npm run build
 
-# 移動到打包資料夾下，若你有調整的話打包後的資料夾請務必調整
+# 移動到打包資料夾下
 cd dist
 
-# 部署到自定義網域
-# echo 'www.example.com' > CNAME
-
+# 初始化新的 git 倉庫
 git init
+
+# 添加 .nojekyll 文件以防止 Jekyll 處理
+touch .nojekyll
+
 git add -A
 git commit -m 'deploy'
 
-# 部署到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# 部署到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-
-# 除此之外，也可以改走 HTTPS 模式
+# 使用 HTTPS 推送到 gh-pages 分支
 git push -f https://github.com/yuch3nchen/vue-notepad.git main:gh-pages
 
 cd -
