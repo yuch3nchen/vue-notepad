b87f940a52b4a63d07333a2d47a3a326342e8900
yuch3n <dustchen1126@gmail.com>
2024-09-06 17:58:37 +0800

    rebuild gh-pages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/vue2-notepad/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/vue-notepad/" : "/",
   transpileDependencies: true,
 });
